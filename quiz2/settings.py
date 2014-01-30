aeca17af218f67057ff8e88de963fa1ed46b7c8c
Scott Hill <scott289@gmail.com>
2014-01-30 11:05:47 -0800

    local settings

--- quiz2/settings.py
+++ quiz2/settings.py
@@ -8,13 +8,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '9)@n1zj@d!qj44vf0thtpwrbdbl(dj_+dkr!a928cg1j(o^0sg'
@@ -83,5 +78,5 @@ STATICFILES_DIRS = (
 )
 
 # Needed for heroku....can't use for local
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config()
