07696d66eaf8aded559eef2f8884d50e1ee1e244
Scott Hill <scott289@gmail.com>
2014-01-30 12:03:36 -0800

    init of reg/login

--- quiz2/apps/quiz/admin.py
+++ quiz2/apps/quiz/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from quiz2.apps.quiz.models import Question, Answer, QuestionAnswer, Tag, QuestionTag
+from quiz2.apps.quiz.models import (Question,
+                                    Answer,
+                                    QuestionAnswer,
+                                    Tag,
+                                    QuestionTag,
+                                    UserProfile)
 
 
 admin.site.register(Question)
@@ -7,3 +12,4 @@ admin.site.register(Answer)
 admin.site.register(QuestionAnswer)
 admin.site.register(Tag)
 admin.site.register(QuestionTag)
+admin.site.register(UserProfile)
--- quiz2/apps/quiz/urls.py
+++ quiz2/apps/quiz/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns(
     url(r'questions/$', views.questions_index, name='questions_index'),
     url(r'question/(?P<question_id>\d+)/$', views.question, name='question'),
     url(r'post_answer/(?P<question_id>\d+)/(?P<qa_id>\d+)/$', views.post_answer, name='post_answer'),
+    url(r'register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
     url(r'^admin/', include(admin.site.urls)),
-
 )
