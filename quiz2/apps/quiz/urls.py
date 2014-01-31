672694fcdda2f483f78a0a54b99567df8b8e6050
Scott Hill <scott289@gmail.com>
2014-01-31 13:30:44 -0800

    moved the login stuff to top view

--- quiz2/apps/quiz/urls.py
+++ quiz2/apps/quiz/urls.py
@@ -12,7 +12,5 @@ urlpatterns = patterns(
     url(r'sets/$', views.questionsets_index, name='questionsets_index'),
 
     url(r'post_answer/(?P<qa_id>\d+)/$', views.post_answer, name='post_answer'),
-    url(r'register/$', views.register, name='register'),
-    url(r'^login/$', views.user_login, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+
 )
--- quiz2/urls.py
+++ quiz2/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from quiz2 import views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,11 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', 'quiz2.apps.quiz.views.home', name='home'),
+
+    url(r'register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^quiz/', include('quiz2.apps.quiz.urls')),
 )
