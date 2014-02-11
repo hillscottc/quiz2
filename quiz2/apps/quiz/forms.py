79055ed924d91bb59dd981e972af77f4ac02f34a
Scott Hill <scott289@gmail.com>
2014-02-11 08:49:52 -0800

    cleanup import

--- quiz2/apps/quiz/forms.py
+++ quiz2/apps/quiz/forms.py
@@ -1,41 +1,42 @@
-from django import forms
+from django.forms import (ModelForm, CharField, PasswordInput, Textarea)
+from django.forms.models import modelformset_factory
 from django.contrib.auth.models import User
 from quiz2.apps.quiz.models import Question, UserProfile, Answer, Quiz
 
 
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+class UserForm(ModelForm):
+    password = CharField(widget=PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
 
 
-class UserProfileForm(forms.ModelForm):
+class UserProfileForm(ModelForm):
     class Meta:
         model = UserProfile
         # fields = ('website', 'picture')
         fields = ('website',)
 
 
-class AnswerForm(forms.ModelForm):
+class AnswerForm(ModelForm):
     class Meta:
         model = Answer
 
 
-class QuizForm(forms.ModelForm):
+class QuizForm(ModelForm):
     class Meta:
         model = Quiz
         exclude = ('created_at', 'updated_at')
 
 
-class QuestionForm(forms.ModelForm):
+class QuestionForm(ModelForm):
     class Meta:
         model = Question
         # exclude = ('created_at', 'updated_at')
         fields = ('text', 'quiz')
         widgets = {
-            'text': forms.Textarea(attrs={'cols': 80, 'rows': 2}),
+            'text': Textarea(attrs={'cols': 80, 'rows': 2}),
         }
 
     def __init__(self, *args, **kwargs):
@@ -44,3 +45,10 @@ class QuestionForm(forms.ModelForm):
         # if not user.is_authenticated():
         #     pass
 
+QuestionFormSet = modelformset_factory(
+        Question,
+        fields=('text',),
+        widgets = {'text': Textarea(attrs={'cols': 80, 'rows': 2}), },
+        can_delete=True,
+        # can_order=True,
+        extra=0)
